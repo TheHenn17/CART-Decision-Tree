8914882d6c6f04168664a06aeebc1f83fdd0ee77
TheHenn17 <44244825+TheHenn17@users.noreply.github.com>
2021-06-27 11:49:29 -0700

    Finished writing the tree and test files

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <chrono>
 #include "Functions/Distance.h"
 #include "Functions/Polynomial.h"
+#include "Functions/ComplexPolynomial.h"
+#include "Functions/Sigmoid.h"
+#include "Functions/Tanh.h"
 
 void generate() {
     srand(time(NULL));
     ofstream outFS;
-    outFS.open("Test_Files/polynomialTests.dat");
+    double total;
+    outFS.open("Tree_Generation_Files/sigmoid.dat");
+    for(double i = -6; i <=6; i = i + .02) {
+        total = 1 / (1 + exp(-i));
+        outFS << i << ", " << total << endl;
+    }
+    outFS.close();
+}
+
+void generate2() {
+    srand(time(NULL));
+    ofstream outFS;
+    double random;
+    outFS.open("Test_Files/sigmoidTests.dat");
     for(int i = 1; i <=100; i++) {
-        outFS << rand() % 500 + 1 << endl;
+        random = (double)rand();
+        random = -6 + random / ((double)RAND_MAX/12);
+        outFS << random << endl;
     }
     outFS.close();
 }
@@ -18,11 +36,15 @@ int main() {
     Function* function = 0;
 
     //generate();
+    //generate2();
 
     cout << "Welcome to Cool Tree Program\n\n";
     cout << "Please enter the function you would like to approximate:\n";
     cout << "\t1. Distance\n";
     cout << "\t2. Polynomial\n";
+    cout << "\t3. Complex Polynomial\n";
+    cout << "\t4. Sigmoid\n";
+    cout << "\t5. Tanh\n";
     cin >> input;
     cout << "\nPlease enter the depth of the tree (make this dynamic later?): ";
     cin >> depth;
@@ -34,6 +56,15 @@ int main() {
             break;
         case 2:
             function = new Polynomial("Tree_Generation_Files/polynomial.dat", "Test_Files/polynomialTests.dat", depth);
+            break;
+        case 3:
+            function = new ComplexPolynomial("Tree_Generation_Files/complexPolynomial.dat", "Test_Files/complexPolynomialTests.dat", depth);
+            break;
+        case 4:
+            function = new Sigmoid("Tree_Generation_Files/sigmoid.dat", "Test_Files/sigmoidTests.dat", depth);
+            break;
+        case 5:
+            function = new Tanh("Tree_Generation_Files/tanh.dat", "Test_Files/tanhTests.dat", depth);
             break;
         default:
             cout << "Error: Invalid input\n";
@@ -93,7 +124,7 @@ int main() {
                 cout << "Time taken by approximating: " << approximationDuration.count() << " microseconds\n";
                 double sum = 0;
                 for(int i = 0; i < computations.size(); i++) {
-                    sum += abs((approximations.at(i)-computations.at(i)))/computations.at(i);
+                    sum += abs((approximations.at(i)-computations.at(i))/computations.at(i));
                 }
                 cout << "Average error: " << sum / computations.size() << endl;
                 break;
